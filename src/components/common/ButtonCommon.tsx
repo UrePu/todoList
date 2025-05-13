89606f338dac61fc6a9e8aed027f139af2c1ec9f
UrePu <darkin31313@gmail.com>
2025-05-13 21:36:52 +0900

    feat: clsx css

--- src/components/common/ButtonCommon.tsx
+++ src/components/common/ButtonCommon.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { clsx } from "clsx";
 
 interface ButtonCommonProps {
   children: string;
   onClick?: () => void;
+  completed?: boolean;
 }
 
-const ButtonCommon = ({ children, onClick }: ButtonCommonProps) => {
+const ButtonCommon = ({ children, onClick, completed }: ButtonCommonProps) => {
   return (
     <button
       onClick={onClick}
-      className="inline-block rounded-md bg-blue-600 px-4 py-2 text-white transition-colors hover:bg-blue-700"
+      className={clsx(
+        "inline-block min-w-[80px] rounded-md px-4 py-2 text-white transition-colors",
+        "bg-blue-600 hover:bg-blue-700",
+        completed === true && "bg-green-400 hover:bg-green-600",
+        completed === false && "bg-red-400 hover:bg-red-600"
+      )}
     >
       {children}
     </button>
