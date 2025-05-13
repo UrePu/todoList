2456171f11a8c12df145f82aa74235bb36eb8f86
UrePu <darkin31313@gmail.com>
2025-05-13 21:36:21 +0900

    feat: complete view

---
+++ src/components/IsDoneView.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useGetCompletedTodos } from "@/lib/hooks/queries/usetGetCompletedTodo";
+import React from "react";
+
+const IsDoneView = () => {
+  const getTodo = useGetCompletedTodos();
+  const { data, isPending, isError } = getTodo;
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error occurred while fetching data.</div>;
+  }
+
+  return (
+    <div>
+      {data?.map((todo, idx) => (
+        <div
+          className="flex w-[50%] justify-start items-center border-b-2 border-gray-300 py-2 gap-2"
+          key={todo.id}
+        >
+          {idx + 1}.<div className="flex ">{todo.title}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default IsDoneView;
