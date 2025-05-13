1e02034e7849e843247e4eae1dbbbdefebce5f20
UrePu <darkin31313@gmail.com>
2025-05-13 23:14:32 +0900

    feat: delete

--- src/components/ViewTable.tsx
+++ src/components/ViewTable.tsx
@@ -3,10 +3,12 @@ import { useGetTodos } from "@/lib/hooks/queries/useGetTodo";
 import React from "react";
 import ButtonCommon from "./common/ButtonCommon";
 import { useSetComplete } from "@/lib/hooks/mutations/useSetComplete";
+import { useDeleteTodo } from "@/lib/hooks/mutations/useDeleteTodo";
 
 const ViewTable = () => {
   const getTodo = useGetTodos();
   const mutate = useSetComplete();
+  const deletemutate = useDeleteTodo();
   const { data, isPending, isError } = getTodo;
 
   if (isPending) {
@@ -18,6 +20,11 @@ const ViewTable = () => {
   const handleClick = (id: string, complete: boolean) => {
     mutate.mutate({ id, completed: !complete });
   };
+  const handleDelete = (id: string) => {
+    if (confirm("정말 삭제하시겠습니까?")) {
+      deletemutate.mutate(id);
+    }
+  };
 
   return (
     <div>
@@ -27,12 +34,20 @@ const ViewTable = () => {
           key={todo.id}
         >
           <div className="flex ">{todo.title}</div>
-          <ButtonCommon
-            onClick={() => handleClick(todo.id, todo.completed)}
-            completed={todo.completed}
-          >
-            {todo.completed ? "완료" : "미완료"}
-          </ButtonCommon>
+          <div className="flex gap-2">
+            <ButtonCommon
+              onClick={() => handleClick(todo.id, todo.completed)}
+              completed={todo.completed}
+            >
+              {todo.completed ? "완료" : "미완료"}
+            </ButtonCommon>
+            <ButtonCommon
+              onClick={() => handleDelete(todo.id)}
+              completed={false}
+            >
+              삭제
+            </ButtonCommon>
+          </div>
         </div>
       ))}
     </div>
