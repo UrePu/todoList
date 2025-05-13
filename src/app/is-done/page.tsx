b6b172299792fc8ad1a6aed6d92ae237f1087201
UrePu <darkin31313@gmail.com>
2025-05-13 23:19:29 +0900

    feat: prefetch

--- src/app/is-done/page.tsx
+++ src/app/is-done/page.tsx
@@ -1,7 +1,27 @@
+import IsDoneView from "@/components/IsDoneView";
+import { getCompletedTodos } from "@/lib/api/getTodos";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import React from "react";
 
-const IsDone = () => {
-  return <div>IsDone</div>;
+const IsDone = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: () => getCompletedTodos(),
+    staleTime: Infinity,
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div>
+        <h1>완료한 일 목록</h1>
+        <IsDoneView />
+      </div>
+    </HydrationBoundary>
+  );
 };
 
 export default IsDone;
