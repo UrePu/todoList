da70345e231ddbdeec1d439c2bc839280673a60b
UrePu <darkin31313@gmail.com>
2025-05-13 23:14:17 +0900

    feat: prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>main</div>;
-}
+import InputForm from "@/components/InputForm";
+import ViewTable from "@/components/ViewTable";
+import { getTodos } from "@/lib/api/getTodos";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const Home = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: () => getTodos(),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="w-full flex flex-col justify-center">
+        <InputForm />
+        <ViewTable />
+      </div>
+    </HydrationBoundary>
+  );
+};
+export default Home;
