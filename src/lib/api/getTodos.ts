26ddf6af7ff414a95f1198ed6a6d54a766ca9248
UrePu <darkin31313@gmail.com>
2025-05-13 23:17:07 +0900

    feat: rename & to env

--- src/lib/api/getTodos.ts
+++ src/lib/api/getTodos.ts
@@ -1,14 +1,16 @@
 import type { TodoType } from "@/types/todos";
 
 export const getTodos = async (): Promise<TodoType[]> => {
-  const res = await fetch("http://localhost:3001/todos");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/todos`);
 
   if (!res.ok) throw new Error("데이터 조회 실패");
   return res.json(); // 배열 반환
 };
 
 export const getCompletedTodos = async (): Promise<TodoType[]> => {
-  const res = await fetch("http://localhost:3001/todos?completed=true");
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/todos?completed=true`
+  );
 
   if (!res.ok) throw new Error("데이터 조회 실패");
   return res.json(); // 배열 반환
