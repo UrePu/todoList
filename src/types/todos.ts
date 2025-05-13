5f7c4e02d471eae5f069f4f0abbb06ecbbe0cfe3
UrePu <darkin31313@gmail.com>
2025-05-13 20:13:29 +0900

    feat: add todo

--- src/types/todos.ts
+++ src/types/todos.ts
@@ -1,7 +1,8 @@
-type TodoType = {
+export type TodoType = {
   id: string;
   title: string;
   completed: boolean;
 };
 
-export type { TodoType };
+// 추가용 타입 (id, completed 제외)
+export type NewTodo = Omit<TodoType, "id" | "completed">;
