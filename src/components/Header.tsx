84e2fd6abf6bc9b75f9fd7cb43280c677566de94
UrePu <darkin31313@gmail.com>
2025-05-13 23:42:04 +0900

    design:header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import LinkCommon from "./common/LinkCommon";
 
 const Header = () => {
   return (
-    <div className="flex gap-4 p-4 bg-gray-200">
-      <LinkCommon href={"/"}>main</LinkCommon>
-      <LinkCommon href={"/is-done"}>is-done</LinkCommon>
+    <div className="w-full flex bg-gray-200 justify-center">
+      <div className="flex w-[80%] gap-4 p-4">
+        <LinkCommon href={"/"}>main</LinkCommon>
+        <LinkCommon href={"/is-done"}>is-done</LinkCommon>
+      </div>
     </div>
   );
 };
